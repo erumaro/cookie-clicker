87e4d89a34071d93ce43a7434470e10246d63c58
Tobias Årud <me@tobiasarud.se>
2016-09-27 11:35:02 +0200

    current progress

--- app.jsx
+++ app.jsx
@@ -5,19 +5,44 @@ class App extends React.Component{
     constructor(){
         super();
         this.state = {
-            clicks: 0
+            clicks: 0,
+            helpers: 0,
+            cost: 10
         };
     }
-    handleClick(e) {
+    handleClick(e){
 		e.preventDefault();
         this.setState({
             clicks: this.state.clicks + 1,
         });
     }
+    addHelper(e){
+      e.preventDefault();
+      var helperCost = Math.floor(10 * Math.pow(1.1,this.state.helpers));
+      if(this.state.clicks >= helperCost){
+        // this.state.helpers = this.state.helpers + 1;
+        this.setState({
+          helpers: this.state.helpers + 1,
+          clicks: this.state.clicks - helperCost,
+          cost: this.state.cost + helperCost,
+        });
+      }
+      if(this.state.helpers >= 1){
+        helperRun();
+      }
+      //var nextCost = Math.floor(10 * Math.pow(1.1, helperCost));
+    }
+    helperRun(){
+      window.setInterval(function(){
+        this.handleClick(this.state.helpers);
+      }, 1000);
+    }
     render(){
         return <div className="container">
-		   <button className="btn btn-primary" onClick={(e) =>this.handleClick(e)}>Skicka</button>
-		   <p>{this.state.clicks}</p>
+		  <button onClick={(e) =>this.handleClick(e)}>Click</button>
+		  <p>{this.state.clicks}</p>
+          <button onClick={(e) =>this.addHelper(e)}>Add helper (Costs: {this.state.cost})</button>
+          <p>{this.state.helpers}</p>
         </div>
     }
 }
